fb839ac9b198cdcecbb49979ebb33e77b80ba92b
Ashley Tolen <tolenashley7@gmail.com>
2018-08-09 01:58:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 // Add
+function add(a, b) {
+  return a + b
+}
 // Subtract
+function subtract(a, b) {
+  return a - b
+}
 // Multiply
+function multiply(a, b) {
+  return a * b
+}
 // Divide
+function divide(a, b) {
+  return a / b
+}
